ecb993068a71fd30f07c6209c04f9afa3ab04d03
c0st1nus <konstantin.koshevoj@bk.ru>
2023-07-03 22:29:16 +0600

    Доделал движение
    пофиксил движение, теперь оно работает

--- Assets/Code/CameraControl.cs
+++ Assets/Code/CameraControl.cs
@@ -4,29 +4,29 @@ using UnityEngine;
 
 public class CameraControl : MonoBehaviour
 {
-    [SerializeField] Transform player;
-    [SerializeField] float sens;
-    float angelX, angelY;
-    public static bool Locked = false;
-
-    void Start()
+    private Transform player;
+    [SerializeField][Range(0.5f, 2f)] float sens = 1;
+    [SerializeField][Range(-20, 0f)] float minAngle = -8;
+    [SerializeField][Range(0f, 20f)] float maxAngle = 8;
+    private void Awake()
     {
-        angelX = transform.eulerAngles.y;
-        angelY = player.eulerAngles.y;
+        player = GameObject.Find("Player").transform;
     }
-
-    void LateUpdate()
+    private void Update()
     {
-        if (!Locked)
-        {
-            Cursor.visible = false;
-            float MouseX = Input.GetAxis("Mouse Y") * sens * -1;
-            float MouseY = Input.GetAxis("Mouse X") * sens;
-            angelX += MouseX;
-            angelX = Mathf.Clamp(angelX, -60, 43);
-            transform.rotation = Quaternion.Euler(angelX, angelY, 0);
-            angelY += MouseY;
-            player.transform.rotation = Quaternion.Euler(0, angelY, 0);
-        }
+        float MouseX = Input.GetAxis("Mouse X") * sens;
+        float MoyseY = Input.GetAxis("Mouse Y") * sens;
+        Vector3 cameraRotation = transform.rotation.eulerAngles;
+        Vector3 playerRotation = player.rotation.eulerAngles;
+
+        cameraRotation.x = (cameraRotation.x > 180) ? cameraRotation.x - 360 : cameraRotation.x;
+        cameraRotation.x = Mathf.Clamp(cameraRotation.x, minAngle, maxAngle);
+        cameraRotation.x -= MoyseY;
+
+        cameraRotation.z = 0;
+        playerRotation.y += MouseX;
+
+        transform.rotation = Quaternion.Euler(cameraRotation);
+        player.rotation = Quaternion.Euler(playerRotation);
     }
 }
--- Assets/Code/MoveControl.cs
+++ Assets/Code/MoveControl.cs
@@ -5,24 +5,20 @@ using UnityEngine;
 public class MoveControl : MonoBehaviour
 {
     private Rigidbody rb;
+    private Vector3 direction;
     public float speed = 1, jumpForce;
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
     }
     private void Update() {
-        if(Input.GetKeyDown(KeyCode.Space)){
-            rb.AddForce(new Vector3(0, jumpForce), ForceMode.Impulse);
-        }
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        direction = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        direction = transform.TransformDirection(direction);
     }
     private void FixedUpdate()
     {
-        float Horizontal = Input.GetAxis("Horizontal");
-        float Vertical = Input.GetAxis("Vertical");
-        Vector3 direction = transform.position;
-        direction.z += Vertical/10;
-        direction.x += Horizontal/10;
-        direction = transform.TransformDirection(direction) * speed;
-        rb.MovePosition(direction);
+        rb.MovePosition(transform.position + direction * Time.deltaTime * speed);
     }
 }
